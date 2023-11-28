1a0fab6484b48218c5b44ab8f8455fb0a5031073
PawiXer <u22epawab@liceum.p.lodz.pl>
2023-11-28 11:40:19 +0100

    added output file

--- converter.py
+++ converter.py
@@ -1,14 +1,17 @@
+def dec_to_bin(value):
+    return value
+
 if __name__ == '__main__':
 
-    with open("input.txt", "r") as input_file:
+    with open("input.txt", "r") as input_file, open("output.txt", "w") as output_file:
         raw_lines = input_file.readlines()
-
-
         numbers = []
         for line in raw_lines:
             numbers.append(int(line.strip()))
 
         print(numbers)
+        for number in numbers:
+            output_file.write(f"{dec_to_bin(number)})
 
 
 
