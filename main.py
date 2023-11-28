be961bc5d16744fb319e9b79fd833a12cc3b6534
PawiXer <u22epawab@liceum.p.lodz.pl>
2023-11-28 10:56:29 +0100

    Introduced number generator

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 if __name__ == '__main__':
-    pass
+    max_value = 10
+    min_value = 2
+    step = 3
+    values = []
+    current = min_value
+
+    while current <= max_value:
+        values.append(current)
+        current += step
+
+    print(values)
+
+
+
 
